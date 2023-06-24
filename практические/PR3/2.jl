bfddd2c6b918855be2c95fb201fba397d5adf634
FenZer4 <113883546+FenZer4@users.noreply.github.com>
2023-06-24 11:14:58 +0300

    Update 2.jl

--- практические/PR3/2.jl
+++ практические/PR3/2.jl
@@ -1,24 +1,3 @@
-#Решето Эратосфена
-function resheto(n::Integer)
-    prime_indexes = ones(Bool, n)
-    prime_indexes[begin] = false
-    i = 2
-    prime_indexes[i^2:i:n] .= false 
-    i = 3
-   
-    while i <= n
-        prime_indexes[i^2:2i:n] .= false
-        i+=1
-        while i <= n && prime_indexes[i] == false
-            i+=1
-        end
-
-    end
-    return findall(prime_indexes)
-end
-
-print(resheto(20))
-
 """Алгоритм решето Эратосфена"""
 function eratosphen(n::Integer)
     is_prime = ones(Bool, n) 
@@ -32,3 +11,6 @@ function eratosphen(n::Integer)
     end
     return (1:n)[is_prime]
 end
+
+
+print(eratosphen(20))
