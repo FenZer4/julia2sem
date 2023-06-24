0557007cc7f17221bfb93a36d8611a0224d8ad62
FenZer4 <113883546+FenZer4@users.noreply.github.com>
2023-06-24 11:18:34 +0300

    Update 5.jl

--- практические/PR4/5.jl
+++ практические/PR4/5.jl
@@ -1,26 +1,4 @@
-#Приведение матрицы к ступеньчатому виду
-function TransformToSteps!(matrix::AbstractMatrix, epsilon::Real = 1e-7)::AbstractMatrix
-	@inbounds for k ∈ 1:size(matrix, 1)
-		absval, Δk = findmax(abs, @view(matrix[k:end,k]))
 
-		(absval <= epsilon) && throw("Вырожденая матрица")
-
-		Δk > 1 && swap!(@view(matrix[k,k:end]), @view(matrix[k+Δk-1,k:end]))
-
-		for i ∈ k+1:size(matrix,1)
-			t = matrix[i,k]/matrix[k,k]
-			@. @views matrix[i,k:end] = matrix[i,k:end] - t * matrix[k,k:end]  
-		end
-	end
-	return matrix
-end
-
-A = [ 1 2 3;
-      0 4 5;
-      0 0 6 ]
-	
-x = TransformToSteps!(A)
-println(x) 
 
 """Приводит матрицу к ступенчатому виду"""
 function toUpperTriangle!(Matrix)
@@ -39,3 +17,10 @@ function toUpperTriangle!(Matrix)
 
     return Matrix
 end
+
+A = [ 1 2 3;
+      0 4 5;
+      0 0 6 ]
+	
+x = toUpperTriangle!(A)
+println(x) 
